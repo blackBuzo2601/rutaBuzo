4552b651a534653f400c25469aa50ed81dbc71c7
blackBuzo2601 <elianpro58@gmail.com>
2024-08-27 20:07:18 -0700

    Primera subida de prueba

---
+++ app.js
@@ -0,0 +1,20 @@
+var numero=0;
+var numeroaleatorio=0;
+
+const abecedario="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+const matriz = [
+    [-3,-3,2,-3,3,-2,-2,1,2,0,2,0,1],
+    [2,3,"I",-1,-1,3,2,0,-3,-3,2,2,1],
+    [1,-3,-3,2,3,1,3,3,2,1,-2,-2,3],
+    [0,0,3,0,3,-3,-2,-3,0,2,2,1,1],
+    [2,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
+  ];
+
+function detectarColumna(){
+    for(let i=0;i<abecedario.length;i++){ //iterará por cada una de las letras (porque puede crecer el array)
+        
+    }
+}
+
+detectarColumna();

2b52095cf806fe2f70a3115ec02c5a1fa4ab8b19
blackBuzo2601 <elianpro58@gmail.com>
2024-08-28 00:09:38 -0700

    Lograda funcion que busca la I en el arreglo, renglon por renglon, empezando por el renglon 0 y recorriendo cada posicion de dicho renglon, si no lo encuentra, pasa al siguiente renglon y se repite el proceso hasta encontrar I

--- app.js
+++ app.js
@@ -3,35 +3,41 @@ var numeroaleatorio=0;
 
 const abecedario="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-const matriz = [
-    [-3,-3,2,-3,3,-2,-2,1,2,0,2,0,1],
-    [2,3,"I",-1,-1,3,2,0,-3,-3,2,2,1],
-    [1,-3,-3,2,3,1,3,3,2,1,-2,-2,3],
-    [0,0,3,0,3,-3,-2,-3,0,2,2,1,1],
-    [4,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-    [2,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-    [3,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-    [2,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-    [1,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-    [2,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-    [5,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-    [7,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-    [5,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-    [1,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-    [4,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
-  ];
+const matriz =[
+[-3,-3,2,-3,3,-2,-2,1,2,0,2,0,1],
+[2,3,'I',-1,-1,3,2,0,-3,-3,2,2,1],
+[1,-3,-3,2,3,1,3,3,2,1,-2,-2,3],
+[0,0,3,0,3,-3,-2,-3,0,2,2,1,1],
+[2,-1,-1,-3,3,3,0,-3,1,-2,2,0,1],
+[0,3,-1,1,-1,-2,2,-2,2,-1,-2,-3,0],
+[0,3,2,0,1,1,2,3,-1,-3,0,0,-2],
+[3,3,-3,-2,3,-3,-1,-3,3,-2,2,-2,-1],
+[-2,-2,1,0,-1,0,3,0,0,-2,2,-3,-1],
+[-3,3,0,-1,-3,1,2,-3,2,-3,0,2,-2],
+[-3,-3,-3,3,-2,0,-2,-3,1,0,1,-1,-2],
+[-1,0,1,2,1,0,'F',0,-3,3,3,-2,-1],
+[1,-3,1,0,1,2,3,1,-2,3,3,0,3]
+]
 let cantidadFilas=matriz.length;
+let cantidadColumnas=matriz[0].length;
 
-function detectarColumna(j){
+
+j=0;
+function detectarColumna(){
     for(let i=0;i<abecedario.length;i++){ //iterará por cada una de las letras (porque puede crecer el array)
-      if(matriz[j][i]==undefined){
+      if(matriz[j][i]=="I"){
+        console.log("El valor Inicial esta en la columna: "+abecedario[i]);
         break;
       }
-      console.log("El valor: "+matriz[j][i]+ " está en la columna "+abecedario[i]);
+      if(i==cantidadColumnas){
+        //console.log("No se encontró I, en esta fila. Pasando a siguiente fila...");
+        j++; //pasar a la siguiente fila
+        detectarColumna();
+      }
     }//concluye for general
 }//concluye detectarColumnaNumero()
 
-function detectarFila(j){
+function detectarFila(){
   for(i=0;i<cantidadFilas;i++){
     if(matriz[i][j]==undefined){
       break;
@@ -44,9 +50,26 @@ function combinacion(){
   detectarFila(j);
 }
 
-j=0;
-
+/*
 for(let l=0;l<15;l++){
   combinacion();
   j++;
 }
+*/
+detectarColumna();
+/*IDEAS GENERALES DE COMO RESOLVERLO
+-Lograr algoritmo que identifique tanto la columna, como la fila en la que esta un elemento.
+-Con ayuda del indicador de filas y columnas, una vez identificada la fila y columna del
+inicio, hay que diseñar un pequeño algoritmo para las posiciones de arriba, abajo, izquierda, derecha:
+ARRIBA=[-1][0]
+ABAJO=[+1][0]
+IZQUIERDA=[0][-1]
+DERECHA=[+1][0]
+-Con ayuda del algoritmo anterior, el cual es para desplazarnos, almacenaremos en un array
+el valor de las cuatro posiciones [0,1,2,3], y luego ocupamos un algoritmo para determinar
+cual de estos 4 numeros, es el mas pequeño (y otra funcion para el mas grande). POr medio
+de banderas, activaremos la bandera de hacia donde nos moveremos, para que en el siguiente 
+movimiento, ignoremos dicha direccion (pues no queremos regresarnos, sino buscar un camino
+  diferente.)
+
+*/

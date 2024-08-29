a6f4d817482e7c408727c0c8b95bb50df33ea10c
blackBuzo2601 <elianpro58@gmail.com>
2024-08-28 19:08:46 -0700

    Codigo comentado, para entender mejor como encuentra la posicion inicial buscando por cada renglon en todas las columnas. Además, se ha implementado para que indique en que letra de las columnas se encuentra

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var numero=0;
 var numeroaleatorio=0;
 
 const abecedario="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                //0123456789...
 
 const matriz =[
 [-3,-3,2,-3,3,-2,-2,1,2,0,2,0,1],
@@ -22,20 +23,31 @@ let cantidadFilas=matriz.length;
 let cantidadColumnas=matriz[0].length;
 
 
-j=0;
-let posicion=[] //almacenara la posisicion mediante renglon,columna.
-function detectarPosicionI(){
-    for(let i=0;i<abecedario.length;i++){ //iterará por cada una de las letras (porque puede crecer el array)
-      if(matriz[j][i]=="I"){
-        posicion.push(j);
-        posicion.push(i);
-        console.log("imprimiendo posicion[renglon,columna]: "+posicion);
+j=0; //valor para la posicion de las filas
+let posicion=[] //Declaramos un array que almacenará la posicion como [renglon],columna]
+
+function detectarPosicionI(){ //buscamos que esta funcion sea reutilizable desde el INICIO del algoritmo.
+    for(let i=0;i<abecedario.length;i++){ //este for iterará por cada letra del abecedario
+
+      if(matriz[j][i]=="I"){ //empieza por el renglon 0 y recorre cada columna, luego sigue al renglon 1
+        //y recorre cada columna, luego sigue al renglon 2 y recorre cada columna, hasta encontrar "I"
+
+        posicion.push(j); //guardar en posicion[]primero la fila = posicion[0]=fila
+        posicion.push(i); //guardar en posicion[]despues la columna=posicion[1]=columna
+        console.log("POSICION INICIAL[renglon,columna]: "+posicion);
+        //para el renglon no hay pierde, pues es nomas el valor de posicion0 del array posicion[], 
+        //pero para la columna, estamos hablando de letras, entonces el numero de la columna debe
+        //referirse a una letra, entonces accedemos a la posicion del abecedario indicando la posicion
+        //de la columna:
+        console.log("La posicion Inicial está en el renglon: "+posicion[0]+" y en la columna: "+abecedario[posicion[1]]);
         break;
+      }else{
+        if(i==cantidadColumnas){ //esto se cumple cuando se llega a la ultima columna (Letra)
+          j++; //incrementar j (que su numero indica en renglon) Pasamos al siguiente renglon
+          detectarPosicionI();
+        }
       }
-      if(i==cantidadColumnas){ //esto se cumple cuando se llega a la ultima columna.
-        j++; //pasar a la siguiente fila
-        detectarPosicionI();
-      }
+      
     }//concluye for general
 }//concluye detectarColumnaNumero()
 
@@ -51,6 +63,7 @@ let abajo=a+1;
 let izquierda=b-1;
 let derecha=b+1;
 
+let reglas=[arriba,abajo,izquierda,derecha];
 let valores=[]; //aqui guardaremos los numeros de del numero arriba,abajo,izquierda y derecha de la posicion.
 let menorNumero=0; //variable que almacenará el numero mas pequeño de las 4 posiciones
 let valoresBoolean=[true,true,true,true];
@@ -62,34 +75,39 @@ function guardarCuatroValores(){
   valores.push(matriz[a][izquierda]);
   valores.push(matriz[a][derecha]);
 
-  
   menorNumero=Math.min(valores[0],valores[1],valores[2],valores[3]);
-  console.log("El menor numero es: "+menorNumero);
+  console.log("El menor numero de las posiciones es: "+menorNumero+ "y está en la dirección: ");
 
+}//concluye funcion guardarCuatroValores
+
+
+function desplazarSigPos(){
+  for(i=0;i<valores.length;i++){
+    if(valores[i]==menorNumero){
+      
+    }
+  }
+}//concluye funcion desplazarSigPos
+
+
+
+guardarCuatroValores();
+
+  /*Nos servirá mas adelante este bloque de aqui
   for(i=0;i<valores.length;i++){
     if(valores[i]==menorNumero){
       valoresBoolean[i]=false;
       console.log("imprimiendo valoresBoolean despues de identificar numero menor: "+valoresBoolean);
+      break;
     }
   }
+*/
 
-  //guardar el indice, para en el otro array, activar bandera en el mismo indice.
-}
-
-guardarCuatroValores();
 
 /*IDEAS GENERALES DE COMO RESOLVERLO
--Lograr algoritmo que identifique tanto la columna, como la fila en la que esta un elemento.
--Con ayuda del indicador de filas y columnas, una vez identificada la fila y columna del
-inicio, hay que diseñar un pequeño algoritmo para las posiciones de arriba, abajo, izquierda, derecha:
-ARRIBA=[-1][0]
-ABAJO=[+1][0]
-IZQUIERDA=[0][-1]
-DERECHA=[+1][0]
--Con ayuda del algoritmo anterior, el cual es para desplazarnos, almacenaremos en un array
-el valor de las cuatro posiciones [0,1,2,3], y luego ocupamos un algoritmo para determinar
-cual de estos 4 numeros, es el mas pequeño (y otra funcion para el mas grande). POr medio
-de banderas, activaremos la bandera de hacia donde nos moveremos, para que en el siguiente 
+
+ 
+Activaremos la bandera de hacia donde nos moveremos, para que en el siguiente 
 movimiento, ignoremos dicha direccion (pues no queremos regresarnos, sino buscar un camino
   diferente.)
 
